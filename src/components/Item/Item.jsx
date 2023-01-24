e028e81ec79e98f6876bb27eb37d19d37ab64301
Guido Forcinito <guidoforci@gmail.com>
2023-01-24 18:34:23 -0300

    .

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,7 +1,7 @@
 export const Item = ({item}) => {
   return (
     <div className="card" style={{width: '18rem'}}>
-            <img src="..." className="card-img-top" alt="..." />
+            <img src="..." className="card-img-top" alt="..."/>
                 <div className="card-body">
     <h5 className="card-title">{item.nombre}</h5>
     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
