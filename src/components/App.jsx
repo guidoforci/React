bea3ac68119105ce6a66be37a69b12142d72f32d
Guido Forcinito <guidoforci@gmail.com>
2023-01-26 12:21:27 -0300

    clase 7

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,13 @@ import './App.css'
 //Componentes
 import Navbar from "./Navbar/Navbar";
 import { ItemListContainer } from './ItemListContainer/ItemListContainer';
+import { ItemDetailContainer } from './ItemDetailContainer/ItemDetailContainer';
 const App = () => {
   return (
     <>
       <Navbar/>
       <ItemListContainer/>
+      <ItemDetailContainer/>
     </>
   );
 }
