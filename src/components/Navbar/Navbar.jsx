1ed59b40de27b20cc4237ed736fb8a0cc577a20d
Guido Forcinito <guidoforci@gmail.com>
2023-02-10 20:03:36 -0300

    .

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
             <div className="container-fluid">
-                <p className="navbar-brand" href="#">CROSSTORE</p>
+                <a className="navbar-brand" href="#">CROSSTORE</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon" />
                 </button>
