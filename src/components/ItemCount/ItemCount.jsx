c99cff3bbc3df5d5a2b05d65bd2e96028514e6d5
Guido Forcinito <guidoforci@gmail.com>
2023-02-14 21:24:09 -0300

    ,

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -1,7 +1,7 @@
 
 import { useState } from "react"
 import { toast } from 'react-toastify'
-
+import { TbShoppingCartPlus } from "react-icons/tb";
 
 export const ItemCount = ({ valInicial, stock, onAdd }) => {
 
@@ -26,7 +26,7 @@ export const ItemCount = ({ valInicial, stock, onAdd }) => {
             <button className="btn btn-primary botonMas" onClick={() => sumar()}>+</button>
             {contador}
             <button className="btn btn-primary botonMenos" onClick={() => restar()}>-</button>
-            <button className="btn btn-primary botonAgregarCarrito" onClick={() => agregarCarrito()}>Agregar al carrito</button>
+            <button className="btn btn-primary botonAgregarCarrito" onClick={() => agregarCarrito()}>Agregar al carrito <TbShoppingCartPlus size="18" /></button>
         </>
     )
 }
