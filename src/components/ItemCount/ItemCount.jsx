c54fef97af48c0adf9259114ed0f640f8048fac3
Guido Forcinito <guidoforci@gmail.com>
2023-02-14 21:10:01 -0300

    .

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -1,13 +1,24 @@
 
 import { useState } from "react"
+import { toast } from 'react-toastify'
 
-export const ItemCount = ({ valInicial, stock }) => {
+
+export const ItemCount = ({ valInicial, stock, onAdd }) => {
 
     const [contador, setContador] = useState(valInicial)
     //Var       //Modificar var     //Estado inicial
 
     const sumar = () => (contador < stock) && setContador(contador + 1) //contador = contador + 1
     const restar = () => (contador > valInicial) && setContador(contador - 1)  //Operador ternario sin else
+    const agregarCarrito = () => {
+        onAdd(contador)
+        toast(`Agregaste ${contador} productos al carrito!`, {
+            position: toast.POSITION.TOP_RIGHT,
+            className: 'toasty',
+            autoClose: 1500,
+            theme: "dark",
+        })
+    }
 
 
     return (
@@ -15,6 +26,7 @@ export const ItemCount = ({ valInicial, stock }) => {
             <button className="btn btn-primary botonMas" onClick={() => sumar()}>+</button>
             {contador}
             <button className="btn btn-primary botonMenos" onClick={() => restar()}>-</button>
+            <button className="btn btn-primary botonAgregarCarrito" onClick={() => agregarCarrito()}>Agregar al carrito</button>
         </>
     )
 }
