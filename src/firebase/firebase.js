cf0d1e2c90c3fb0b99a03066ea5d467ffee325bb
Guido Forcinito <guidoforci@gmail.com>
2023-02-23 15:45:44 -0300

    .

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,7 +4,6 @@ import {getFirestore, collection, doc, addDoc, getDoc, getDocs, updateDoc, delet
 
 const firebaseConfig = {
     apiKey: process.env.API_KEY,
-    //"AIzaSyAs8sg9c05ZDbFWLAU4bT2P9Y5ymOZEHTE",
     authDomain: "curso-react---guido-forcinito.firebaseapp.com",
     projectId: "curso-react---guido-forcinito",
     storageBucket: "curso-react---guido-forcinito.appspot.com",
